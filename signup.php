b202a730bb45e1c621a8b91aad28106a6ed415ee
bimantaraaryasatya <bimantara_aryasatya_32rpl@student.smktelkom-mlg.sch.id>
2024-03-20 22:27:39 +0700

    menambahkan fitur registrasi yang sudah terkoneksi oleh db dan menambah satu file functions.php untuk menyimpan fungsi yang bisa diakses oleh file lainnya

--- signup.php
+++ signup.php
@@ -1,3 +1,14 @@
+<?php
+    require "functions.php";
+    if (isset($_POST["register"])){
+        if(registrasi($_POST) > 0){
+            echo "<script> alert('user baru berhasil ditambahkan! ') </script>";
+        } else{
+            echo mysqli_error($conn);
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +16,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up Page</title>
     <link rel="stylesheet" href="signup_style.css">
+    <style>
+        label {
+            display: block;
+            font-weight: bold;
+            font-size: 12px;
+        }
+    </style>
 </head>
 <body>
     <div class="sign-up-container">
@@ -12,21 +30,20 @@
             <h1>Sign Up</h1>
         </div>
         <div class="main-content">
+            <form action="" method="post"> <!-- Tambahkan form di sini -->
+                <label for="username">Username :</label>
+                <input type="text" name="username" id="username">
+                
+                <label for="password">Password :</label>
+                <input type="password" name="password" id="password">
 
-            <h4>Username</h4>
-            <input type="text" name="username" id="username">
+                <label for="password">Konfirmasi Password :</label>
+                <input type="password" name="password2" id="password2">
             
-            <h4>Email</h4>
-            <input type="email" name="email" id="mail">
-
-            <h4>Password</h4>
-            <input type="password" name="password" id="password">
-
-            <h4>Confirm Password</h4>
-            <input type="password" name="confirm-password" id="confirm-password">
-        </div>
-        <div class="sign-up-btn">
-            <a href="signin.php" class="btn">Sign Up</a>
+                <button type="submit" name="register" class="sign-up-btn" style="border: 1px solid #6563ff;">
+                    <a href="" class="btn">Sign Up</a>
+                </button> <!-- Beri nama dan type pada tombol -->
+            </form>
         </div>
         <div class="to-signin">
             <p>already have an account? <a href="signin.php" class="sign-in">Sign In</a></p>
