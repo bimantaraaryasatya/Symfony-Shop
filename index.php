f90c1a9598fa34e2a2f2c559a9f880e186ea0735
bimantaraaryasatya <bimantara_aryasatya_32rpl@student.smktelkom-mlg.sch.id>
2024-04-02 01:23:39 +0700

    nambah dikit, lanjut besok

--- index.php
+++ index.php
@@ -3,16 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="homepage_style.css">
+    <link rel="stylesheet" href="homepage_sstyle.css">
     <title>Home</title>
 </head>
 <body>
     <?php
         include"navbar.php";
     ?>
-    <h1>Test</h1>
-    <div class="carousel">
-        
+    
+    <div class="main_content">
+        <div class="left-content">
+            <h2>Symfony Shop</h2>
+            <h1>Rhythm of Life, Sound Your Melody</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec velit nec sem congue commodo. Fusce tempor, elit ut </p>
+            <div class="button">
+
+            </div>
+        </div>
+
+        <div class="right-content">
+            <div class="image-home">
+                <img src="" alt="">
+            </div>
+        </div>
     </div>
 </body>
 </html>
