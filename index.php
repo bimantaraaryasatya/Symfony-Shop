b42bd8e8d65b120031f7f346c5b51cea3d33f566
bimantaraaryasatya <bimantara_aryasatya_32rpl@student.smktelkom-mlg.sch.id>
2024-04-02 19:28:40 +0700

    setor ui index.php dan mencoba fitur baru yaitu menampilkan barang acak

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="homepage_sstyle.css">
+    <link rel="stylesheet" href="homepage_style.css">
     <title>Home</title>
 </head>
 <body>
@@ -11,21 +11,70 @@
         include"navbar.php";
     ?>
     
-    <div class="main_content">
-        <div class="left-content">
-            <h2>Symfony Shop</h2>
-            <h1>Rhythm of Life, Sound Your Melody</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec velit nec sem congue commodo. Fusce tempor, elit ut </p>
-            <div class="button">
+    <div class= "main-content">
+        <!-- First Page !-->
 
+        <div class="home-content">
+            <div class="left-content">
+                <h2>Symfony Shop</h2>
+                <h1>Rhythm of Life, Sound Your Melody</h1>
+                <p>Creative music store with a complete collection of musical instruments and audio <br> equipment, as well as an inspiring community space.</p>
+                <div class="button">
+                    <a href="#" class="product-home-btn">Our Product</a>
+                    <a href="#" class="call-home-btn">Our Product</a>
+                </div>
+            </div>
+
+            <div class="right-content">
+                <div class="image-home">
+                    <img src="../images/home_page5.jpg" alt="">
+                </div>
             </div>
         </div>
 
-        <div class="right-content">
-            <div class="image-home">
-                <img src="" alt="">
+        <!-- Second Page !-->
+
+        <div class="product-mix-content">
+            <div class="product-mix-title">
+                <h2>Our Product</h2>
             </div>
+
+            <?php
+                require "functions.php";
+
+                //menghitung total baris dalam colom
+                $result = mysqli_query($conn, "SELECT COUNT(*) AS total FROM barang");
+                $row = mysqli_fetch_assoc($result);
+                $totalRows = $row["total"];
+
+                if ($totalRows < 4) {
+                    echo "tidak cukup data";
+                }else{
+                    //generate 4 id acak
+                    $randomIds = [];
+                    while (count($randomIds) < 4){
+                        $randomId = mt_rand(1, $totalRows);
+                        if (!in_array($randomId, $randomIds)){
+                            $randomIds[] = $randomId;
+                        }
+                    }
+
+                    $queryBarang_Acak = mysqli_query($conn, "SELECT * FROM barang WHERE id_Barang IN (" . implode(',', $randomIds) . ")");
+
+                    while ($dataBarang_Acak = mysqli_fetch_array($queryBarang_Acak)){
+            ?>
+                        <div class="card">
+                            <img src="images/<?=$dataBarang_Acak['foto_Barang']?>">
+                            <p><?=$dataBarang_Acak['nama_Barang']?></p>
+                            <p>IDR <?=number_format($dataBarang_Acak['harga_Barang'], 0, ',', '.')?></p>
+                        </div>
+            <?php
+
+                    }
+                }
+            ?>
         </div>
+
     </div>
 </body>
 </html>
