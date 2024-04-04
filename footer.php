fd81f844febc30a24e2bba3d2a503c441f77e3b1
bimantaraaryasatya <bimantara_aryasatya_32rpl@student.smktelkom-mlg.sch.id>
2024-04-04 22:23:29 +0700

    add animation on the footer

--- footer.php
+++ footer.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="footer_style.css">
     <script src="https://kit.fontawesome.com/5e57207f98.js" crossorigin="anonymous"></script>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <title>Footer</title>
 </head>
 <body>
@@ -12,52 +13,56 @@
         <div class="footer">
           <div class="main">
             <div class="list">
-              <h4>Produk Kami</h4>
+              <h4 data-aos="zoom-in-right" data-aos-duration=1000 data-aos-once="true">Produk Kami</h4>
               <ul>
-                <li><a href="#">Gitar Elektrik</a></li>
-                <li><a href="#">Bass Gitar</a></li>
-                <li><a href="#">Drum</a></li>
-                <li><a href="#">Keyboard</a></li>
-                <li><a href="#">Microphone</a></li>
+                <li data-aos="fade-right" data-aos-duration=1000 data-aos-delay=200 data-aos-once="true"><a href="#">Gitar Elektrik</a></li>
+                <li data-aos="fade-right" data-aos-duration=1000 data-aos-delay=400 data-aos-once="true"><a href="#">Bass Gitar</a></li>
+                <li data-aos="fade-right" data-aos-duration=1000 data-aos-delay=600 data-aos-once="true"><a href="#">Drum</a></li>
+                <li data-aos="fade-right" data-aos-duration=1000 data-aos-delay=800 data-aos-once="true"><a href="#">Keyboard</a></li>
+                <li data-aos="fade-right" data-aos-duration=1000 data-aos-delay=1000 data-aos-once="true"><a href="#">Microphone</a></li>
               </ul>
             </div>
     
             <div class="list">
-              <h4>Tentang Kami</h4>
+              <h4 data-aos="zoom-in-right" data-aos-duration=1000 data-aos-once="true">Tentang Kami</h4>
               <ul>
-                <li><a href="#">Tentang Symfony Shop</a></li>
-                <li><a href="#">Lokasi di Google Maps</a></li>
+                <li data-aos="fade-right" data-aos-duration=1000 data-aos-delay=200 data-aos-once="true"><a href="#">Tentang Symfony Shop</a></li>
+                <li data-aos="fade-right" data-aos-duration=1000 data-aos-delay=400 data-aos-once="true"><a href="#">Lokasi di Google Maps</a></li>
               </ul>
             </div>
     
             <div class="list">
-              <h4>Hubungi Kami</h4>
+              <h4 data-aos="zoom-in-right" data-aos-duration=1000 data-aos-once="true">Hubungi Kami</h4>
               <ul>
-                <li><a href="https://www.google.com/maps/place/Jl.+Raya+Sawojajar,+Sawojajar,+Kec.+Kedungkandang,+Kota+Malang,+Jawa+Timur/@-7.9772034,112.6545466,16.25z/data=!4m6!3m5!1s0x2dd62843213b3e3b:0xb122e6ed58f7609b!8m2!3d-7.9736408!4d112.6558499!16s%2Fg%2F1hm5nw0h4?entry=ttu" target="_blank">Jl. Raya Sawojajar</a></li>
-                <li><a href="https://www.google.com/maps/place/Malang,+Kota+Malang,+Jawa+Timur/@-7.9786372,112.5492092,12z/data=!3m1!4b1!4m6!3m5!1s0x2dd62822063dc2fb:0x78879446481a4da2!8m2!3d-7.9666204!4d112.6326321!16zL20vMDR3OGRu?entry=ttu" target="_blank">Kota Malang</a></li>
-                <li><a href="#">+62 813-3096-2030</a></li>
-                <li><a href="#">+62 821-1606-2233</a></li>
-                <li><a href="#">symfonyshop@gmail.com</a></li>
+                <li data-aos="fade-right" data-aos-duration=1000 data-aos-delay=200 data-aos-once="true"><a href="https://www.google.com/maps/place/Jl.+Raya+Sawojajar,+Sawojajar,+Kec.+Kedungkandang,+Kota+Malang,+Jawa+Timur/@-7.9772034,112.6545466,16.25z/data=!4m6!3m5!1s0x2dd62843213b3e3b:0xb122e6ed58f7609b!8m2!3d-7.9736408!4d112.6558499!16s%2Fg%2F1hm5nw0h4?entry=ttu" target="_blank">Jl. Raya Sawojajar</a></li>
+                <li data-aos="fade-right" data-aos-duration=1000 data-aos-delay=400 data-aos-once="true"><a href="https://www.google.com/maps/place/Malang,+Kota+Malang,+Jawa+Timur/@-7.9786372,112.5492092,12z/data=!3m1!4b1!4m6!3m5!1s0x2dd62822063dc2fb:0x78879446481a4da2!8m2!3d-7.9666204!4d112.6326321!16zL20vMDR3OGRu?entry=ttu" target="_blank">Kota Malang</a></li>
+                <li data-aos="fade-right" data-aos-duration=1000 data-aos-delay=600 data-aos-once="true"><a href="#">+62 813-3096-2030</a></li>
+                <li data-aos="fade-right" data-aos-duration=1000 data-aos-delay=800 data-aos-once="true"><a href="#">+62 821-1606-2233</a></li>
+                <li data-aos="fade-right" data-aos-duration=1000 data-aos-delay=1000 data-aos-once="true"><a href="#">symfonyshop@gmail.com</a></li>
               </ul>
             </div>
     
             <div class="list">
-              <h4>Sosial Media</h4>
+              <h4 data-aos="zoom-in-right" data-aos-duration=1000 data-aos-once="true">Sosial Media</h4>
               <div class="social">
-                <a href=""><i class="fa-brands fa-facebook"></i></a>
-                <a href=""><i class="fa-brands fa-instagram"></i></a>
-                <a href=""><i class="fa-brands fa-x-twitter"></i></a>
-                <a href=""><i class="fa-brands fa-youtube"></i></a>
+                <a href="" data-aos="fade-up" data-aos-duration=1000 data-aos-once="true"><i class="fa-brands fa-facebook"></i></a>
+                <a href="" data-aos="fade-up" data-aos-duration=1000 data-aos-delay=200 data-aos-once="true"><i class="fa-brands fa-instagram"></i></a>
+                <a href="" data-aos="fade-up" data-aos-duration=1000 data-aos-delay=400 data-aos-once="true"><i class="fa-brands fa-x-twitter"></i></a>
+                <a href="" data-aos="fade-up" data-aos-duration=1000 data-aos-delay=600 data-aos-once="true"><i class="fa-brands fa-youtube"></i></a>
               </div>
             </div>
             
           </div>
         </div>
     
-        <div class="end-text">
+        <div class="end-text" data-aos="fade-up" data-aos-duration=800 data-aos-once="true">
           <p>Copyright © 2024 Symfony Shop. Hak & Cipta Terlindungi</p>
         </div>
         
       </section>
+      <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+      <script>
+        AOS.init();
+      </script>
 </body>
 </html>
