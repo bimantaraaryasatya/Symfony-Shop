34fe9132c8957cb86bea0bdb963a7d0d4aa10a43
bimantaraaryasatya <bimantara_aryasatya_32rpl@student.smktelkom-mlg.sch.id>
2024-04-17 14:59:15 +0700

    sementara

--- semuaproduk.php
+++ semuaproduk.php
@@ -1,3 +1,7 @@
+<?php
+    require "functions.php"
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +11,31 @@
     <title>All Product</title>
 </head>
 <body>
-    
+    <?php
+        include "navbar.php";
+    ?>
+
+    <div class="all-product">
+        <div class="title">
+            <?php
+                $id_Kategori = $_GET['id-kategori'];
+                $queryProduk = mysqli_query($conn, "SELECT * FROM barang WHERE `id-Kategori` = $id_Kategori");
+            ?>
+            <h1>All Product</h1>
+        </div>
+        <div class="container">
+            <?php
+                while($dataProduk = mysqli_fetch_array($queryProduk)){
+            ?>
+                    <div class="card">
+                        <img src="images/<?=$dataProduk['foto_Barang']?>" alt="">
+                        <p><?= $dataProduk['nama_Barang']?></p>
+                        <p>IDR <?=number_format($dataProduk['harga_Barang'], 0, ',', '.')?></p>
+                    </div>
+            <?php 
+                }
+            ?>
+        </div>
+    </div>
 </body>
 </html>
